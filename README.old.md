833a24201ae2a02bae01ed34b92cceb2d44c6fb5
kirill-k88 <kirill-k88@ya.ru>
2024-02-27 15:34:14 +0300

    init cra

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# valatis-test
---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../Header/Header'
+import './App.css'
+
+export function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
